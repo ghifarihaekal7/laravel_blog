df295f943ca404726519eb3decf879469bf17a1e
ghifarihaekal7 <ghifari_haekal@apps.ipb.ac.id>
2019-06-24 10:46:39 +0700

    Modified welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome, Peasants
                 </div>
 
                 <div class="links">
