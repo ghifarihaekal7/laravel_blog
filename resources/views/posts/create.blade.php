464a6f404a9f60155153816299f06aba0924c920
ghifarihaekal7 <ghifari_haekal@apps.ipb.ac.id>
2019-07-05 11:00:20 +0700

    Created create post page

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,20 @@
+@extends('main')
+
+@section('title', '| Create New Post')
+@section('content')
+  <div class="row justify-content-center">
+    <div class="col-md-8 col-md-offset-2">
+      <h1>Create New Post</h1>
+      <hr>
+      {!! Form::open(array('route' => 'posts.store')) !!}
+        {{ Form::label('title', 'Title:') }}
+        {{ Form::text('title', null, array('class' => 'form-control')) }}
+
+        {{ Form::label('content', 'Post Content:') }}
+        {{ Form::textarea('content', null, array('class' => 'form-control')) }}
+
+        {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block')) }}
+      {!! Form::close() !!}
+    </div>
+  </div>
+@endsection
