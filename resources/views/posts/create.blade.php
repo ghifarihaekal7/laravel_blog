6e87377aa06976eda02f62f074da001de952cc51
ghifarihaekal7 <ghifari_haekal@apps.ipb.ac.id>
2019-07-08 10:28:17 +0700

    Added parsley.js validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,20 +1,29 @@
 @extends('main')
 
 @section('title', '| Create New Post')
+
+@section('stylesheets')
+  {!! Html::style('css/parsley.css') !!}
+@endsection
+
 @section('content')
   <div class="row justify-content-center">
     <div class="col-md-8 col-md-offset-2">
       <h1>Create New Post</h1>
       <hr>
-      {!! Form::open(array('route' => 'posts.store')) !!}
+      {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
         {{ Form::label('title', 'Title:') }}
-        {{ Form::text('title', null, array('class' => 'form-control')) }}
+        {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
 
         {{ Form::label('body', 'Post Content:') }}
-        {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+        {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
 
         {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top:10px;')) }}
       {!! Form::close() !!}
     </div>
   </div>
+@endsection
+
+@section('scripts')
+  {!! Html::script('js/parsley.min.js') !!}
 @endsection
